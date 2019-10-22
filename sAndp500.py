55bf1f61072b205a0043d8103ee9c6fb36b79121
unknown <jongwoo2@illinois.edu>
2019-10-22 23:11:06 +0900

    gather company pricing data with yahoo finance

--- sAndp500.py
+++ sAndp500.py
@@ -1,7 +1,21 @@
+#BeautifulSoup is for5 HTML parsing; getting tickers/symbols from wikipedia
+#Pickle for saving the s&p 500 list
 import bs4 as bs
 import pickle
 import requests
+import datetime as dt
+import os
+import pandas as pd
+# import pandas_datareader.data as web
+import yfinance as yf
+from pandas_datareader import data as pdr
 
+yf.pdr_override()
+
+
+#td stands for table data
+#tr stands for table row
+#find table name that contains data from wiki source: "wikitable sortable"
 def save_sp500_tickers():
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
@@ -9,11 +23,40 @@ def save_sp500_tickers():
     tickers = []
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text
+        ticker = ticker.strip('\n')    #required since every element within tickers contain '\n'
+        ticker = str(ticker).replace('.','-') #some tickers with dots and dashes causes problem while searching
         tickers.append(ticker)
 
     with open("sp500tickers.pickle","wb") as f:
         pickle.dump(tickers,f)
 
+    print(tickers)
     return tickers
 
-save_sp500_tickers()
+# save_sp500_tickers()
+# create stock_dfs folder before else throws EOF error
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime.now()
+    for ticker in tickers:
+        print(ticker)
+        # just in case your connection breaks, we'd like to save our progress!
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = pdr.get_data_yahoo(ticker, start, end)
+            # df.reset_index(inplace=True)
+            # df.set_index("Date", inplace=True)
+            # df = df.drop("Symbol", axis=1)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+
+
+get_data_from_yahoo()
