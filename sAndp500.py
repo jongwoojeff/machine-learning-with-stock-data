9704a45c332f091a2189ee2791eb2c659754d641
jeff <jongwoo2@illinois.edu>
2019-10-24 14:59:43 +0900

    compile pulled s&p data

--- sAndp500.py
+++ sAndp500.py
@@ -58,5 +58,29 @@ def get_data_from_yahoo(reload_sp500=False):
         else:
             print('Already have {}'.format(ticker))
 
+# get_data_from_yahoo()
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+    main_df = pd.DataFrame()
 
-get_data_from_yahoo()
+    for count,ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close': ticker}, inplace=True) #renaming adj close column to whatever ticker is
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+
+compile_data()
