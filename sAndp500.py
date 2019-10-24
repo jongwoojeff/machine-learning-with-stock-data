8e33c16ce8c46f2a91651349626e28f42bc6420b
unknown <jongwoo2@illinois.edu>
2019-10-24 21:43:53 +0900

    machine learning against sp 500

--- sAndp500.py
+++ sAndp500.py
@@ -12,6 +12,11 @@ from pandas_datareader import data as pdr
 import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
+from collections import Counter
+from sklearn import svm, neighbors
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 
 style.use('ggplot')
 
@@ -125,8 +130,68 @@ def process_data_for_labels(ticker):
     tickers = df.columns.values.tolist()
     df.fillna(0, inplace=True)
 
+    #price change percentage in 7 days
     for i in range(1,hm_days+1):
         df['{}_{}d'.format(ticker,i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
 
     df.fillna(0, inplace=True)
     return tickers, df
+
+#if the prices rises more than 2 percent by the next 7 days, buy. Else sell or hold.
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02 #if stock price change by 2 percent
+    for col in cols:
+        if col > requirement:
+            return 1 #buy
+        if col < -requirement:
+            return -1 #sell
+    return 0 # hold
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map(buy_sell_hold, 
+    df['{}_1d'.format(ticker)],
+    df['{}_2d'.format(ticker)],
+    df['{}_3d'.format(ticker)],
+    df['{}_4d'.format(ticker)],
+    df['{}_5d'.format(ticker)],
+    df['{}_6d'.format(ticker)],
+    df['{}_7d'.format(ticker)]))
+
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+
+    df.fillna(0, inplace = True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace = True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change() #normalize; percent change within a day
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace = True)
+
+    X = df_vals.values       #featuresets
+    Y = df['{}_target'.format(ticker)].values     #labels
+
+    return X, Y, df
+def do_ml(ticker):
+    X, Y, df = extract_featuresets(ticker)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25)
+
+    # clf = neighbors.KNeighborsClassifier() #simple classifier
+    clf = VotingClassifier([('lsvc',svm.LinearSVC()),
+    ('knn',neighbors.KNeighborsClassifier()),
+    ('rfor',RandomForestClassifier())]) #3 classifiers vote
+
+    clf.fit(X_train, Y_train)
+    confidence = clf.score(X_test, Y_test)
+    print('Accuracy:', confidence)
+    predictions = clf.predict(X_test)
+    print('Predicted sprea:', Counter(predictions))
+
+    return confidence
+
+do_ml('BAC')
